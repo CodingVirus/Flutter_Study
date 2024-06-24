36749b25285be001167c4a1d8d4d0c743f4d3e69
CodingVirus <twominyong@gmail.com>
2024-06-24 23:17:28 +0900

    카테고리 화면에 더미 데이터 정보 넘기기

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -2,17 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import '../models/category.dart';
 import 'meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'some title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -36,7 +41,9 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: (){_selectCategory(context);},
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
             ),
         ],
       ),
