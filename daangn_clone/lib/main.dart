c97e9d194641f3c2fc04e1abb8b5d58cec07adb6
CodingVirus <twominyong@gmail.com>
2024-07-12 23:09:27 +0900

    당근마켓 클론 앱 폴더 추가

---
+++ daangn_clone/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: const Row(
+          children: [
+            SizedBox(width: 16),
+            Text(
+              '중앙동',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        leadingWidth: 100,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.search, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu_rounded, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.bell, color: Colors.black),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("home page"),
+      ),
+    );
+  }
+}
